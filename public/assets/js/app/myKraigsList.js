a6d014ace7e19f665feb1f5b85e6a70b6a1dfd91
Robert Kraig <robert.kraig@5thkind.com>
2013-11-03 00:48:56 -0700

    updating file formating.

--- public/assets/js/app/myKraigsList.js
+++ public/assets/js/app/myKraigsList.js
@@ -1,127 +1,141 @@
-var app = angular.module("myKraigsList", ['ngRoute', 'ngResource']);
+var app = angular.module("myKraigsList", [
+	'ngRoute',
+	'ngResource'
+]);
 
-app.config(['$routeProvider','$locationProvider', function($routeProvider, $locationProvider)
-{
-	var routeHandler = {
-		templateUrl: 'assets/templates/sites.html',
-		controller: 'pageCtrlr'
-	}
-	$routeProvider.
-		when('/stuff', routeHandler).
-		when('/jobs', routeHandler).
-		when('/gigs', routeHandler).
-		when('/places', routeHandler).
-		when('/services', routeHandler).
-		otherwise({
-			redirectTo: '/'
-		});
-	$locationProvider.html5Mode(true)
-}]);
+app.config([
+	'$routeProvider','$locationProvider',
+		function($routeProvider, $locationProvider)
+		{
+			var routeHandler = {
+				templateUrl: 'assets/templates/sites.html',
+				controller: 'pageCtrlr'
+			}
+			$routeProvider.
+				when('/stuff', routeHandler).
+				when('/jobs', routeHandler).
+				when('/gigs', routeHandler).
+				when('/places', routeHandler).
+				when('/services', routeHandler).
+				otherwise({
+					redirectTo: '/'
+				});
+			$locationProvider.html5Mode(true)
+		}
+]);
 
 app.factory('Session',function()
 {
 	return {title:'My Kraigs List'};
 });
 
-app.controller('headerCtrlr',[ '$scope', '$location', 'Session', function($scope, $location, Session)
-{
-	$scope.$on('$routeChangeStart', function(scope, next, current)
-	{
-		console.log($location.path());
-		var path = $location.path();
-		switch(true)
+app.controller('headerCtrlr',[
+	'$scope', '$location', 'Session',
+		function($scope, $location, Session)
 		{
-			case /stuff/.test(path):
-				$scope.site = 'stuff';
-				break;
-			case /jobs/.test(path):
-				$scope.site = 'jobs';
-				break;
-			case /gigs/.test(path):
-				$scope.site = 'gigs';
-				break;
-			case /places/.test(path):
-				$scope.site = 'places';
-				break;
-			case /services/.test(path):
-				$scope.site = 'services';
-				break;
+			$scope.$on('$routeChangeStart', function(scope, next, current)
+			{
+				console.log($location.path());
+				var path = $location.path();
+				switch(true)
+				{
+					case /stuff/.test(path):
+						$scope.site = 'stuff';
+						break;
+					case /jobs/.test(path):
+						$scope.site = 'jobs';
+						break;
+					case /gigs/.test(path):
+						$scope.site = 'gigs';
+						break;
+					case /places/.test(path):
+						$scope.site = 'places';
+						break;
+					case /services/.test(path):
+						$scope.site = 'services';
+						break;
+				}
+				Session.site = $scope.site;
+				Session.title = $scope.title;
+			});
+
+			$scope.gotoStuff = function()
+			{
+				$location.path('/stuff');
+			};
+
+			$scope.gotoJobs = function()
+			{
+				$location.path('/jogs');
+			};
+
+			$scope.gotoGigs = function()
+			{
+				$location.path('/gigs');
+			};
+
+			$scope.gotoPlaces = function()
+			{
+				$location.path('/places');
+			};
+
+			$scope.gotoServices = function()
+			{
+				$location.path('/services');
+			};
+
 		}
-		Session.site = $scope.site;
-		Session.title = $scope.title;
-	});
+]);
 
-	$scope.gotoStuff = function()
-	{
-		$location.path('/stuff');
-	};
+app.controller('pageCtrlr',[
+	'$scope','$http','Session',
+		function($scope, $http, Session)
+		{
+			$scope.isLoaded = false;
+			console.log(Session.site);
+			$http.post('/sites/data',{
+				site:Session.site
+			}).success(function(json)
+			{
+				console.log(json);
+				$scope.title = json.page_info.title;
+				$scope.pagetitle = json.page_info.pagetitle;
+				$scope.fields = json.fields;
+				$scope.pagesearchexample = json.page_info.pagesearchexample;
+				$scope.area_list = json.area_list;
+				$scope.region_list = json.region_list;
+				$scope.isLoaded = true;
+			});
 
-	$scope.gotoJobs = function()
-	{
-		$location.path('/jogs');
-	};
+			$scope.isAreaListOpen = false;
+			$scope.isRegionListOpen = false;
 
-	$scope.gotoGigs = function()
-	{
-		$location.path('/gigs');
-	};
+			$scope.openRegions = function()
+			{
+				$scope.isRegionListOpen = !$scope.isRegionListOpen;
+			};
 
-	$scope.gotoPlaces = function()
-	{
-		$location.path('/places');
-	};
+			$scope.openAreas = function()
+			{
+				$scope.isAreaListOpen = !$scope.isAreaListOpen;
+			};
 
-	$scope.gotoServices = function()
-	{
-		$location.path('/services');
-	};
+			$scope.selectAreas = function($element)
+			{
+				console.log($element);
+			};
 
-}]);
+			$scope.submit = function()
+			{
 
-app.controller('pageCtrlr',['$scope','$http','Session',function($scope, $http, Session)
-{
-	$scope.isLoaded = false;
-	console.log(Session.site);
-	$http.post('/sites/data',{
-		site:Session.site
-	}).success(function(json)
-	{
-		console.log(json);
-		$scope.title = json.page_info.title;
-		$scope.pagetitle = json.page_info.pagetitle;
-		$scope.fields = json.fields;
-		$scope.pagesearchexample = json.page_info.pagesearchexample;
-		$scope.area_list = json.area_list;
-		$scope.region_list = json.region_list;
-		$scope.isLoaded = true;
-	});
+			};
+		}
+]);
 
-	$scope.isAreaListOpen = false;
-	$scope.isRegionListOpen = false;
-
-	$scope.openRegions = function()
-	{
-		$scope.isRegionListOpen = !$scope.isRegionListOpen;
-	};
-
-	$scope.openAreas = function()
-	{
-		$scope.isAreaListOpen = !$scope.isAreaListOpen;
-	};
-
-	$scope.selectAreas = function($element)
-	{
-		console.log($element);
-	};
-
-	$scope.submit = function()
-	{
-
-	};
-}]);
-
-app.controller('myContentCtrlr',['$scope',function($scope)
-{
-	$scope.closeAll = function(){};
-	$scope.showSearch = function(){};
-}]);
+app.controller('myContentCtrlr',[
+	'$scope',
+		function($scope)
+		{
+			$scope.closeAll = function(){};
+			$scope.showSearch = function(){};
+		}]);
